271c7255640038adbea08e30810a20192c34d3a6
krismawati <krismawatti11@gmail.com>
2022-06-20 11:30:08 +0700

    ParkingFloor Model

---
+++ app/Models/ParkingFloor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ParkingFloor extends Model
+{
+    use HasFactory;
+}
