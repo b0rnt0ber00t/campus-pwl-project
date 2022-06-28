6226de93be413bb0a280a96751fd0a71ef4573a8
b0rnt0ber00t <lanabackend@gmail.com>
2022-06-28 21:15:39 +0700

    change park template

--- resources/views/parking/index.blade.php
+++ resources/views/parking/index.blade.php
@@ -8,11 +8,11 @@
   <div class="card">
     <div class="card-body">
       <div class="card-title">Floor #parking_floor.floor</div>
-      <div class="card border-primary mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
-        <div class="card-body text-primary">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <div class="row">
+        <div class="col-lg-2">
+          <div class="card bg-primary">
+            <div class="card-header">Parking #parking_floor.parking.number</div>
+          </div>
         </div>
       </div>
     </div>
